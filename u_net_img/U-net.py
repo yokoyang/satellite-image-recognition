28affd1ff257e0e40e72624be15313adad0000fd
yokoyang <yokocc255@163.com>
2018-04-02 14:41:27 +0800

    encapsulation class

--- u_net_img/U-net.py
+++ u_net_img/U-net.py
@@ -4,6 +4,7 @@ import random
 
 import cv2
 import keras
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tifffile as tiff
@@ -26,6 +27,8 @@ class U_net(object):
         self.edge_size = int((self.crop_size - self.patch_size) / 2)
         self.get_size = 144
         self.scale_size = self.patch_size * self.n_split
+        self.test_file_dir = '/home/yokoyang/PycharmProjects/untitled/896_val'
+        self.msk_file_dir = '/home/yokoyang/PycharmProjects/untitled/predict_img_result'
 
     @property
     def scale_size(self):
@@ -113,6 +116,12 @@ class U_net(object):
         msk_img[:, :] = msk[:, :, 1]
         msk_img ^= 1
         return msk_img
+
+    def get_image_row(self, image_id, dir_name):
+        filename = os.path.join(
+            self.file_dir, dir_name, '{}.tif'.format(image_id))
+        img = tiff.imread(filename)
+        return img
 
     def get_image(self, image_id):
         filename = os.path.join(
@@ -507,21 +516,31 @@ class U_net(object):
         conv10 = BatchNormalization(axis=1)(conv10)
         conv10 = keras.layers.advanced_activations.ELU()(conv10)
 
-        up11 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_uniform')(
+        up11 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_uniform')(
             UpSampling2D(size=(2, 2))(conv10))
         merge11 = concatenate([conv1, up11], axis=3)
+        conv11 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_uniform')(merge11)
+        conv11 = BatchNormalization(axis=1)(conv11)
+        conv11 = keras.layers.advanced_activations.ELU()(conv11)
 
+        conv11 = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_uniform')(conv11)
+        conv11 = Cropping2D(cropping=((self.edge_size, self.edge_size), (self.edge_size, self.edge_size)))(conv11)
 
-        crop9 = Cropping2D(cropping=((self.edge_size, self.edge_size), (self.edge_size, self.edge_size)))(conv10)
-        conv9 = BatchNormalization(axis=1)(crop9)
-        conv9 = keras.layers.advanced_activations.ELU()(conv9)
+        conv11 = BatchNormalization(axis=1)(conv11)
+        conv11 = keras.layers.advanced_activations.ELU()(conv11)
 
-        conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+        conv12 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_uniform')(conv11)
 
-        model = Model(inputs=inputs, outputs=conv10)
+        model = Model(inputs=inputs, outputs=conv12)
         model.compile(optimizer=Nadam(lr=1e-3), loss=self.jaccard_coef_loss,
                       metrics=['binary_crossentropy', self.jaccard_coef_int])
         return model
+
+    def get_image_without_msk(self, image_id, dir_name):
+        img = self.get_image_row(image_id, dir_name)
+        img = img.astype(np.float32) / 255
+        img_RGB = cv2.resize(img, (self.scale_size, self.scale_size))
+        return img_RGB
 
     def get_model_by_name(self, model_name):
         if model_name == "unet2":
@@ -551,7 +570,7 @@ class U_net(object):
                 print("loaded")
                 model.load_weights(last_weight)
 
-            check_point_file_name = str(loop_i) + name_class + '_2.hdf5'
+            check_point_file_name = str(loop_i) + name_class + '.hdf5'
             model_checkpoint = ModelCheckpoint(check_point_file_name, monitor='val_jaccard_coef_int',
                                                save_best_only=True,
                                                mode='max')
@@ -576,9 +595,162 @@ class U_net(object):
             if loop_i != 0:
                 print("loaded")
                 model.load_weights(last_weight)
-            check_point_file_name = str(loop_i) + '_rotate_val_jaccard_coef_int_building.hdf5'
+            check_point_file_name = str(loop_i) + name_class + '.hdf5'
             model_checkpoint = ModelCheckpoint(check_point_file_name, monitor='val_jaccard_coef_int',
                                                save_best_only=True,
                                                mode='max')
             model.fit(x_trn, y_trn, batch_size=16, epochs=epochs, verbose=1, shuffle=True, callbacks=[model_checkpoint],
                       validation_data=(x_val, y_val))
+
+    def predict_id(self, img, model, th, dir_name):
+        prd = np.zeros((self.patch_size * self.n_split, self.patch_size * self.n_split, 1)).astype(np.float32)
+
+        for i in range(self.n_split):
+            for j in range(self.n_split):
+                x = img[self.patch_size * i:self.patch_size * (i + 1), self.patch_size * j:self.patch_size * (j + 1), :]
+                x = self.reflect_img(x)
+                tmp = model.predict(x[None, :, :, :], batch_size=4)
+                prd[self.patch_size * i:self.patch_size * (i + 1), self.patch_size * j:self.patch_size * (j + 1)] = tmp
+        prd_result = prd > th
+        return prd_result
+
+    def check_predict(self, model, th, img_id, dir_name, name_class='general_building'):
+        msk_prd, row = self.predict_id(img_id, model, th, dir_name)
+        img = self.get_image(img_id)
+        msk_prd = msk_prd[:, :, 0]
+        msk = self.get_mask(img_id, name_class)
+        plt.figure(figsize=[21, 8])
+
+        plt.subplot(1, 3, 1)
+        plt.title('Training Image')
+        plt.imshow(img)
+        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cv2.imwrite("img_result/train" + img_id + ".tif", img2)
+
+        plt.subplot(1, 3, 2)
+        plt.title('Training Mask')
+        plt.imshow(msk, cmap=plt.get_cmap('gist_ncar'))
+        # cv2.imwrite("msk.tif", msk)
+
+        plt.subplot(1, 3, 3)
+        plt.title('Predicted Mask')
+        plt.imshow(msk_prd, cmap=plt.get_cmap('gist_ncar'))
+        # cv2.imwrite("msk_prd.tif", msk_prd)
+        plt.axis('off')
+        plt.show()
+
+    def generate_all_model_result_in_one_pic(self, general_building,tree_model,water_model,bare_land_1_model,
+                                             building_yard_model,countryside_model,factory_model,playground_model,
+                                             road_model,shadow_model,dir_name = "shanghai2"):
+        dic_class = dict()
+        dic_class['water'] = [48, 93, 254]
+        dic_class['tree'] = [12, 169, 64]
+        dic_class['playground'] = [102, 17, 151]
+        dic_class['road'] = [111, 111, 111]
+        dic_class['building_yard'] = [255, 255, 255]
+        dic_class['bare_land'] = [239, 156, 119]
+        dic_class['general_building'] = [249, 255, 25]
+        dic_class['countryside'] = [227, 22, 33]
+        dic_class['factory'] = [48, 254, 254]
+        dic_class['shadow'] = [255, 1, 255]
+
+        test_img = pd.read_csv(self.test_file_dir + '/data_imageID.csv')
+        test_img_Image_ID = sorted(test_img.ImageId.unique())
+
+        for d in range(0, 81):
+            img_id = test_img_Image_ID[d]
+            img = self.get_image_without_msk(img_id, dir_name)
+            img = self.post_normalize_image(img)
+            # 1
+            build_msk = self.predict_id(img=img, model=general_building, th=0.5, dir_name=dir_name)
+            build_msk = build_msk[:, :, 0]
+
+            width, height = build_msk.shape
+            channel = 3
+
+            # 2
+
+            tree_msk = self.predict_id(img=img, model=tree_model, th=0.5, dir_name=dir_name)
+            tree_msk = tree_msk[:, :, 0]
+
+            # 3
+
+            water_msk = self.predict_id(img=img, model=water_model, th=0.5, dir_name=dir_name)
+            water_msk = water_msk[:, :, 0]
+
+            # 4
+
+            bare_land_msk = self.predict_id(img=img, model=bare_land_1_model, th=0.5, dir_name=dir_name)
+            bare_land_msk = bare_land_msk[:, :, 0]
+
+            # 5
+
+            building_yard_msk = self.predict_id(img=img, model=building_yard_model, th=0.5, dir_name=dir_name)
+            building_yard_msk = building_yard_msk[:, :, 0]
+
+            # 6
+
+            countryside_msk = self.predict_id(img=img, model=countryside_model, th=0.5, dir_name=dir_name)
+            countryside_msk = countryside_msk[:, :, 0]
+
+            # 7
+
+            factory_msk = self.predict_id(img=img, model=factory_model, th=0.5, dir_name=dir_name)
+            factory_msk = factory_msk[:, :, 0]
+
+            # 8
+
+            playground_msk = self.predict_id(img=img, model=playground_model, th=0.5, dir_name=dir_name)
+            playground_msk = playground_msk[:, :, 0]
+
+            # 9
+
+            road_msk = self.predict_id(img=img, model=road_model, th=0.5, dir_name=dir_name)
+            road_msk = road_msk[:, :, 0]
+
+            # 10
+
+            shadow_msk = self.predict_id(img=img, model=shadow_model, th=0.5, dir_name=dir_name)
+            shadow_msk = shadow_msk[:, :, 0]
+
+            row_img = self.get_image_row(img_id, dir_name)
+            row_img = cv2.resize(row_img, (self.scale_size, self.scale_size))
+
+            merge_msk = np.copy(row_img)
+            for i in range(width):
+                for j in range(height):
+
+                    if road_msk[i][j]:
+                        merge_msk[i][j] = dic_class['road']
+                    elif water_msk[i][j]:
+                        merge_msk[i][j] = dic_class['water']
+
+                    elif build_msk[i][j]:
+                        merge_msk[i][j] = dic_class['general_building']
+
+                    elif shadow_msk[i][j]:
+                        merge_msk[i][j] = dic_class['shadow']
+
+                    elif bare_land_msk[i][j]:
+                        merge_msk[i][j] = dic_class['bare_land']
+                    elif building_yard_msk[i][j]:
+                        merge_msk[i][j] = dic_class['building_yard']
+
+                    elif playground_msk[i][j]:
+                        merge_msk[i][j] = dic_class['playground']
+
+                    elif countryside_msk[i][j]:
+                        merge_msk[i][j] = dic_class['countryside']
+                    elif factory_msk[i][j]:
+                        merge_msk[i][j] = dic_class['factory']
+                    elif tree_msk[i][j]:
+                        merge_msk[i][j] = dic_class['tree']
+
+            print("finished")
+            msk_file_name = self.msk_file_dir + "/" + img_id + ".tif"
+            tiff.imsave(msk_file_name, merge_msk)
+            del img, merge_msk, water_msk, road_msk, countryside_msk, factory_msk, playground_msk, build_msk, shadow_msk, \
+                bare_land_msk, tree_msk,
+            K.clear_session()
+            gc.collect()
+        pass
